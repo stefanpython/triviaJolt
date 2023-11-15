1af02b4a086d5483c60eac17bb0ba65abd5c6b31
stefanpython <stefanhtml@yahoo.com>
2023-11-15 18:29:39 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Trivia from "./components/Trivia";
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Welcome to Trivia</h1>
+      <Trivia />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Trivia.jsx
@@ -0,0 +1,11 @@
+import "./Trivia.css";
+
+function Trivia() {
+  return (
+    <div className="Trivia">
+      <h3>Trivia Component</h3>
+    </div>
+  );
+}
+
+export default Trivia;
